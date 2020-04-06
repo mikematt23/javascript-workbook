6aeb83a5ac2ea1ec50cb96342670380e25c9ffc0
Michael MAtteis <michaelcmatteis@gmail.com>
2020-04-05 19:12:49 -0500

    adding my code for ticTacToe

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -13,6 +13,12 @@ let board = [
 ];
 
 let playerTurn = 'X';
+let horizonResult = false;
+let horizonPiece = " "
+let verticalResult = false
+let verticalPiece = " "
+let diagonalResult = false
+let diagonalPiece = " "
 
 function printBoard() {
   console.log('   0  1  2');
@@ -25,22 +31,78 @@ function printBoard() {
 
 function horizontalWin() {
   // Your code here
+
+  for (let i = 0; i< board.length;i++) {
+    if (board[i][0] === "X" && board[i][1] === "X" && board[i][2] === "X" ) {
+      horizonResult = true;
+      horizonPiece = "Player X wins"
+    } else if (board[i][0] === "O" && board[i][1] === "O" && board[i][2] === "O" ){
+      horizonResult = true;
+      horizonPiece = "Player O wins"
+    }
+  }
 }
 
 function verticalWin() {
   // Your code here
+  for(let i = 0; i < board.length; i++){
+    if(board [0][i] === "X" && board[1][i] === "X" && board[2][i] === "X"){
+      verticalResult = true;
+      verticalPiece = "Player X wins"
+    } else if (board [0][i] === "O" && board[1][i] === "O" && board[2][i] === "O"){
+      verticalResult = true;
+      verticalPiece = " Player O Wins"
+    }
+  }
 }
 
 function diagonalWin() {
   // Your code here
+  if(
+    board[0][0] === "X" && board [1][1] === "X" && board[2][2] === "X" ||
+    board[0][2] === "X" && board[1][1] === "X" && board[2][0] === "X"){
+      diagonalResult = true;
+      diagonalPiece = "player X Wins"
+  } else if(
+    board[0][0] === "O" && board [1][1] === "O" && board[2][2] === "O" ||
+    board[0][2] === "O" && board[1][1] === "O" && board[2][0] === "O"){
+      diagonalResult = true;
+      diagonalPiece = "Player O wins"
+    }
 }
 
 function checkForWin() {
   // Your code here
+  horizontalWin()
+  if (horizonResult) {
+    console.log(horizonPiece)
+  }
+  verticalWin()
+  if (verticalResult){
+    console.log(verticalPiece)
+  }
+  diagonalWin()
+  if (diagonalResult){
+    console.log(diagonalPiece)
+  }
+  
 }
 
 function ticTacToe(row, column) {
   // Your code here
+
+let check = board[row] [column];
+
+if(check.includes("X" || check.includes("O"))){
+  console.log('invalid move')
+} else {
+  board[row][column] = playerTurn
+  if(playerTurn === "X"){
+    playerTurn = "O"
+  } else {
+    playerTurn = "X"
+  }
+}  checkForWin();
 }
 
 function getPrompt() {
