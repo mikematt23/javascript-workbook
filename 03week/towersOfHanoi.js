4237c4837e9cee5f768d4ae531f80089e7d0bcab
Michael MAtteis <michaelcmatteis@gmail.com>
2020-04-18 19:31:59 -0500

    pushing up the towers game

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -13,30 +13,67 @@ let stacks = {
   c: []
 };
 
+
 function printStacks() {
   console.log("a: " + stacks.a);
   console.log("b: " + stacks.b);
   console.log("c: " + stacks.c);
 }
 
-function movePiece() {
+function movePiece(startStack,endStack, result) {
   // Your code here
-
+  console.log(result,startStack, endStack)
+  let lastFrom = startStack.pop();
+  let lastTo = endStack.pop()
+  if(result ==  true){
+    if(lastTo != null) {
+       endStack.push(lastTo);
+    }
+    endStack.push(lastFrom);
+  } else { 
+    startStack.push(lastFrom);
+    endStack.push(lastTo);
+  }
 }
 
-function isLegal() {
+function isLegal(startStack, endStack) {
   // Your code here
-
+  let r = false;
+  let lastFrom = startStack.pop();
+  let lastTo = endStack.pop(); 
+  
+  if(lastFrom < lastTo){
+    r = true;
+  } else if (lastFrom > lastTo){
+    r = false;
+    console.log("You can Not put a bigger number on a smaller one. Please make another move")
+  } else if(lastTo == null && (lastFrom === 1|| lastFrom === 2 || lastFrom === 3 || lastFrom === 4)){
+    r = true;
+  } else if(lastFrom == null && lastTo == null){
+    r = false;
+    console.log("Invalid Movie, Please Try again.")
+  } 
+  startStack.push(lastFrom);
+  endStack.push(lastTo);
+  return r
 }
 
 function checkForWin() {
   // Your code here
-
+  let win = "4,3,2,1"
+  if(stacks.b.toString() === win||
+     stacks.c.toString() === win){
+       console.log("Congrts!!!! You Won!")
+     }
 }
 
 function towersOfHanoi(startStack, endStack) {
   // Your code here
-
+  let fromStack = stacks[startStack];
+  let toStack =stacks[endStack];
+  let result = isLegal(fromStack, toStack);
+  movePiece(fromStack, toStack, result);
+  checkForWin();
 }
 
 function getPrompt() {
