819ac3852c52bb6c60f9daca5dacaaa06cdea0a7
Michael MAtteis <michaelcmatteis@gmail.com>
2020-05-09 21:22:01 -0500

    adding a hover event

--- 07week/adressBook/adressBook.js
+++ 07week/adressBook/adressBook.js
@@ -12,12 +12,30 @@ let people = function(){
   }) 
   .then(function(data){
      console.log(data)
-     data.map()
+     processContacts(data.results)
   })
 }
 
-// let getName = function(person){
-//   console.log(person.getName)
+function processContacts(contacts){
+  console.log(contacts)
+  contacts.forEach(function(contact){
+    let firstName = contact.name.first
+    let lastName = contact.name.last
+    let friendsList = document.getElementById("friends")
+    let friend = document.createElement("li")
+    friend.innerText = `${firstName}  ${lastName}`
 
-// }
+    friend.addEventListener("mouseenter", function showAddress(){
+     let city = contact.location.city
+     friend.innerText = `this person lives in the city of ${city}`
+     friend.style.color = "orange"
+    })
+
+    friend.addEventListener('mouseleave',function(){
+      friend.innerText = `${firstName}  ${lastName}`
+      friend.style.color = ""
+    })
+    friendsList.appendChild(friend)
+  })
+}
 
