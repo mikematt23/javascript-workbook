54fe15afeaeedfb958ef4d3d2295d301f8bf2f46
Michael MAtteis <michaelcmatteis@gmail.com>
2020-05-12 22:23:10 -0500

    adding style

---
+++ 07week/typingGame/game.js
@@ -0,0 +1,37 @@
+'use strict'
+
+window.onload= function(){
+people()
+}
+
+let people = function(){
+  fetch('https://randomuser.me/api/?results=10')
+  .then(function(response){
+    return response.json()
+  }) 
+  .then(function(data){
+     processContacts(data.results)
+  })
+}
+let body = document.getElementById("body")
+function processContacts(contacts){
+  console.log(contacts)
+  contacts.forEach(function(contact){
+    let name = contact.name.first + " " + contact.name.last
+    let friend = document.createElement('li')
+    let img = document.createElement("img")
+    let button = document.createElement("button")
+    button.innerText = "Click to RoboFi"
+    let span = document.createElement("span")
+
+    span.innerText = name
+    friend.appendChild(img)
+    friend.appendChild(button)
+    friend.appendChild(span)
+    body.appendChild(friend)
+
+    button.addEventListener("click", function(){
+      img.src = 'https://robohash.org/'+ name
+    })
+  })
+}
